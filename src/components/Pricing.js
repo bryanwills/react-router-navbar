96a0163722655f95bc4427af87e1cb6c6718b626
Bryan Wills <bryanwi09@gmail.com>
2022-11-11 14:55:54 -0500

    first commit

---
+++ src/components/Pricing.js
@@ -0,0 +1,33 @@
+export default function Pricing(){
+    return(
+    <div>
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        <h1>Pricing</h1><br />
+        
+
+    </div>
+    )
+}
